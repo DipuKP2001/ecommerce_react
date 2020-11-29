1f67dc75d9f02a70ced4ce1f41d3b7a36a99a123
DipuKP2001 <dipukp602@gmail.com>
2020-11-29 13:42:23 +0530

    products layout complete

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -10,9 +10,11 @@ const products = [
 const Products = () => {
     return (
         <main>
-            <Grid>
+            <Grid container justify="center" spacing={4}>
                 {products.map((product)=> (
-                    <Product product={product} />
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
                 ))}
             </Grid>
         </main>
